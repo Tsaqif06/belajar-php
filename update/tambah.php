ffec6d7056c78c5544cb60625e7151337b97d265
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-01-26 16:45:09 +0700

    update data

---
+++ update/tambah.php
@@ -0,0 +1,64 @@
+<?php
+require './functions.php';
+
+// cek tombol submit ditekan atau tidak
+$submitted = false;
+if(isset($_POST["submit"])) {
+    
+
+
+    // cek apakah data berhasil atau tidak
+    if(tambah($_POST) > 0) {
+        $submitted = true;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data</title>
+</head>
+<body>
+    <h1>Tambah Data</h1>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td><label for="nama">Nama</label></td>
+                <td><input type="text" id="nama" name="nama" required></td>
+            </tr>
+            <tr>
+                <td><label for="nrp">NRP</label></td>
+                <td><input type="text" id="nrp" name="nrp" required></td>
+            </tr>
+            <tr>
+                <td><label for="email">Email</label></td>
+                <td><input type="email" id="email" name="email" required></td>
+            </tr>
+            <tr>
+                <td><label for="jurusan">Jurusan</label></td>
+                <td><input type="text" id="jurusan" name="jurusan" required></td>
+            </tr>
+            <t>
+                <td><label for="gambar">Gambar</label></td>
+                <td><input type="text" id="gambar" name="gambar" required></td>
+            </t>
+        </table>
+        <br>
+        <button type="submit" id="submit" name="submit">Kirim</button>
+        <p>
+        <?php
+        if($submitted == true) {
+            echo "Submitted✅";
+        } else if ($submitted == false && isset($_POST['submit'])){
+            echo "Error❌";
+        }
+        ?>
+        </p>
+        <a href="./index.php">Kembali</a>
+    </form>
+</body>
+</html>
